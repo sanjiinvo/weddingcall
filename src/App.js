9c0f5a10914c9c5025487cbede8abfff55950d2e
sanjiinvo <sanzhar_sattybaev@mail.ru>
2023-09-18 20:12:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useRef, useState } from 'react';
+import './app.scss';
+import heartgull from './img/fullheart.png'
+import { CaretUpFill } from "react-bootstrap-icons";
+import toporn from './img/toporn.jpg'
 function App() {
+  const [convOpen, setconvOpen] = useState(false)
+
+  const openWings = () =>{
+    setconvOpen(!convOpen)
+
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className= {`left-wing wings ${convOpen? 'left-wing-open': ''}`} >
+        <div  className='heart-left' onClick={openWings}>
+          <p className='heart-title'>click me</p>
+        </div>
+      </div>
+      <div className={`right-wing wings ${convOpen? 'right-wing-open': ''}`}>
+
+      </div>
+
+      <div className='conv-shape'>
+      <div className='first-block'>
+          <div className='first-titles'>
+            <p className='first-block-title'>
+            Құралай
+            </p>
+            <p className='first-block-title-2'>
+            Қыз Ұзату
+            </p>
+          </div>
+      </div>
+      <div className='second-block'>
+
+      </div>
+
+
+
+      </div>
     </div>
   );
 }
